9dd68698f240f13d81b156ce85347b1ce63a33d3
Shuo Wu <2289993431@qq.com>
2024-11-25 17:56:29 +0800

    Next.js 不支持 TypeScript 格式的配置文件，将其改为 JavaScript 格式

---
+++ next.config.js
@@ -0,0 +1,23 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'picsum.photos',
+        pathname: '/**',
+      }
+    ],
+  },
+  productionBrowserSourceMaps: false,
+  swcMinify: true,
+  compress: true,
+  poweredByHeader: false,
+  experimental: {
+    optimizeCss: true,
+    scrollRestoration: true,
+  },
+  output: 'standalone',
+}
+
+module.exports = nextConfig 
